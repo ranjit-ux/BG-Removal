b28ba7b42d04a7ee360bca9633c12d824c4581d1
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2026-01-10 16:13:47 +0530

    Fixing vercel crash

--- Backend/index.js
+++ Backend/index.js
@@ -1,67 +1,34 @@
-// import express from "express"
-// import cors from "cors"
-// import dotenv from "dotenv"
-// import connectDB from "./configs/mongodb.js";
-// import userRouter from "./routes/userRoutes.js";
-
-// dotenv.config();
-
-// const app=express();
-
-// app.use('/api/user/webhook', userRouter)
-
-// const PORT=process.env.PORT || 3000;
-
-// await connectDB();
-
-// app.use(cors());
-
-
-// app.use(express.json());
-
-// app.get('/', (req,res)=>{
-//     res.send("API Working")
-// })
-
-// // app.use('/api/user/webhook', userRouter)
-
-
-// //APP Listen
-// app.listen(PORT, ()=>{
-//     console.log(`Server running on Port ${PORT}`);
-// })
-
-
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
 import connectDB from "./configs/mongodb.js";
-import { clerkWebhook } from "./controllers/userController.js";
 import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
-const app = express();
+const app=express();
+
+app.use('/api/user/webhook', userRouter)
+
+const PORT=process.env.PORT || 3000;
+
+await connectDB();
 
 app.use(cors());
 
-// Webhook MUST come before express.json
-app.post(
-  "/api/user/webhook",
-  express.raw({ type: "application/json" }),
-  clerkWebhook
-);
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  await connectDB();
-  res.send("API Working");
-});
+app.get('/', (req,res)=>{
+    res.send("API Working")
+})
 
-app.use("/api/user.webhook", async (req, res, next) => {
-  await connectDB();
-  // next();
-}, userRouter);
+// app.use('/api/user/webhook', userRouter)
 
-export default app;
+
+//APP Listen
+app.listen(PORT, ()=>{
+    console.log(`Server running on Port ${PORT}`);
+})
+
+
